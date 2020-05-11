1ddb42a9621360c9ac25b86644fdc013eb31e45e
Ryan Werner <rmwerner@umich.edu>
2020-05-11 14:24:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function shout(string) {return string.toUpperCase()}
-function whisper(string) {return string. toLowerCase()}
-function logShout(string) {string.toUpperCase console.log()}
+function whisper(string) {return string.toLowerCase()}
+function logShout(string) {console.log(string.toUpperCase)}
