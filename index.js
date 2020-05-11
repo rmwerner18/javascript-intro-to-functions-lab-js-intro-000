1f39203b0024611ac088ecd6ba1a2b1daef437e4
Ryan Werner <rmwerner@umich.edu>
2020-05-11 15:02:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
+function sayHiToGrandma(string) {if(var string === string.toLowerCase) {console.log("I can't hear you!")}
+else if(var string === string.toUpperCase) {console.log("YES INDEED")}
+else if(var string === "I love you grandma.") {console.log("I love you too.")}}
+
